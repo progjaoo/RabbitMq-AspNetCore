c6f01d0a34a19425d77045fe81eced0b863823b7
Joao marcos valente <joaomarcoslars.81@gmail.com>
2023-10-11 20:27:25 -0300

    Feat: Testando o RabbitMq e suas bibliotecas

---
+++ RabbitMQClient.Marketing.API/Subscribers/CustomerCreatedSubscriber.cs
@@ -0,0 +1,57 @@
+﻿using MessagingEvents.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQClient.Marketing.API.Subscribers
+{
+    public class CustomerCreatedSubscriber : IHostedService
+    {
+        private readonly IModel _channel;
+
+        const string EXCHANGE = "rabbitmq";
+        const string CUSTOMER_CREATED_QUEUE = "cursorabbitmq-client";
+
+        public CustomerCreatedSubscriber()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            var connection = connectionFactory.CreateConnection("curso-rabbitmq-client-consumer");
+
+            _channel = connection.CreateModel();
+
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var contentArray = eventArgs.Body.ToArray();
+                var contentString = Encoding.UTF8.GetString(contentArray);
+
+                var @event = JsonSerializer.Deserialize<CustomerCreated>(contentString);
+
+                Console.WriteLine($"Messagem recebida: {contentString}");
+
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(CUSTOMER_CREATED_QUEUE, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
+
